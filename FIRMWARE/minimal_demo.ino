4282c3f88084beb840c5517a572428aa1b204b0b
Clément G. <gaillard.clement.37@proton.me>
2026-09-20 19:54:21 +0200

    Update minimal_demo.ino

--- FIRMWARE/minimal_demo.ino
+++ FIRMWARE/minimal_demo.ino
@@ -29,39 +29,47 @@ void setup() {
 
   digitalWrite (DATA, LOW);
   digitalWrite (CLOCK, LOW);
+
   digitalWrite (N_LATCH_EN, HIGH);
   digitalWrite (N_POLARITY, HIGH);
 
-  delay (150);
+  delay (300);
 
   digitalWrite (VBIAS_EN, HIGH);
-
-  delay(150);
+  Serial.begin(115200);
+  delay (8000);    // to make sure I can actually upload a new sketch before it enables HV.
 }
 
 void loop() {
-  /* Expected procedure:
 
-  shiftData16 (<16 bit variable>);
-  delay (5);
-  pushDataLatch();
-  delay (5);
   HV_ENABLE();
-  activateArrayAC (1000, 1000) actuate the droplet for 1s at 1kHz
+  delay (4000);
 
-  ...
+ 
+  /*
+  The electrodes are identified like this:
+        6   /   8   /   10   /   12
+        5   /   7   /   9    /   13
+        4   /   3   /   11   /   14
+        2   /   1   /   0    /   15
+*/
 
-  HV_DISABLE();
-  shiftData16 (0b0000000000000000);
-  pushDataLatch();
+ // Moving the droplet in circles
 
-  */
-
-}
+  
+  moveCell (8);
+  moveCell (10);
+  moveCell (12);
+  moveCell (13);
+  moveCell (14);
+  moveCell (11);
+  moveCell (3);
+  moveCell (7);
+  }
 
 
 void shiftData16 (uint16_t data) {
-  for (uint8_t b = 0; b <16; b++) {
+  for (uint8_t b = 0; b < 16; b++) {
     digitalWrite (DATA, (data >> b) & 0b0000000000000001);
     delayMicroseconds (1);
     digitalWrite (CLOCK, HIGH);
@@ -88,20 +96,32 @@ void HV_ENABLE () {
 
 
 void HV_DISABLE () {
-  pinMode (BLD_IO, INPUT); // clamping `HV OUT` to GND 
+  pinMode (BLD_IO, INPUT); // clamping `HV OUT` to GND
   digitalWrite (HVBOOST_EN, LOW); // disabling the HV BOOST
   delay (750); // discharging the HV output capacitors
 }
 
-void activateArrayAC(uint16_t freq, uint32_t duration){   //ex: duration: 1000ms (1s) freq: 1000Hz (1kHz)
-  if (freq == 0 || freq >=65000) return;
-  
+void activateArrayAC(uint16_t freq, uint32_t duration) {  //ex: duration: 1000ms (1s) freq: 1000Hz (1kHz)
+  if (freq == 0 || freq >= 65000) return;
   uint32_t timeStamp = millis();
   uint32_t halfPeriod = (uint32_t) (500000UL / freq);
 
   while (millis() - timeStamp <= duration) {
-  pol = !pol;
-  digitalWrite(N_POLARITY, pol);
-  delayMicroseconds (halfPeriod);
+    pol = !pol;
+    digitalWrite(N_POLARITY, pol);
+    delayMicroseconds (halfPeriod);
   }
+  digitalWrite(N_POLARITY, HIGH);
+}
+
+void moveCell (uint8_t cell) {        // The AC frequency actuation is implemented here directly by constantly changing the register values.
+                                      // unlike toggling the POL pin, it doesn't change all the electrodes LOW (0V) to HIGH (168V) 
+                                      // drawbacks: The actuation frequency can't be as high as if it was just toggling the POL pin, as the whole register needs to be rewritten
+  for (uint16_t i = 0; i < 300; i++) {
+      shiftData16 (0x0001 << cell);
+      pushDataLatch();
+      delay(3);
+      shiftData16 (0x0000);
+      pushDataLatch();
+    }
 }
